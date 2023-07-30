18d1416bcc3555a25ef5a9883a934570727391d9
NakedCabbage <yangjeffery1238@gmail.com>
2023-07-29 18:35:13 -0700

    error fixed

--- art.py
+++ art.py
@@ -4,6 +4,6 @@ import random
 chars = r'\|/'
 
 def draw(rows, columns):
-    for r in rows:
+    for r in range(rows):
         print(''.join(random.choice(chars) for _ in range(columns)))
 draw(10, 20)
